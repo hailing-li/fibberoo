7cc735abc7d334a09c50a154945ca48ddc8f4261
hailing-li <hailing@hawaii.edu>
2017-02-07 10:22:09 -1000

    change for green checks

--- fibberoo.js
+++ fibberoo.js
@@ -3,16 +3,16 @@
  * Created by hailingli on 2/7/2017.
  */
 
-function fib(){
-  let result=[];
-  let p1=0;
-  let p2=1;
-  result.push(p1,p2);
-  for (let i=0; i<98; i++){
-    let current =p1+p2;
+function fib() {
+  const result = [];
+  let p1 = 0;
+  let p2 = 1;
+  result.push(p1, p2);
+  for (let i = 0; i < 98; i += 1) {
+    const current = p1 + p2;
     result.push(current);
-    p1=p2;
-    p2=current;
+    p1 = p2;
+    p2 = current;
   }
   return result;
 }
